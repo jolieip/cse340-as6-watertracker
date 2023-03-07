389567019a507d820f47a2df9681a2979bdb291f
jolieip <ipjolie1507@gmail.com>
2023-03-07 15:35:21 -0800

    nav bar

--- app/src/main/java/cse340/finalproject/MainActivity.java
+++ app/src/main/java/cse340/finalproject/MainActivity.java
@@ -1,14 +1,52 @@
 package cse340.finalproject;
 
+import java.io.*;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        // define your fragments here
+        final Fragment fragment1 = new HomeFragment();
+        final Fragment fragment2 = new HistoryFragment();
+        final Fragment fragment3 = new SettingsFragment();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottonnav);
+
+        // handle navigation selection
+        bottomNavigationView.setOnItemSelectedListener(
+                new NavigationBarView.OnItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment fragment;
+                        switch (item.getItemId()) {
+                            case R.id.homeFrag:
+                                fragment = fragment1;
+                                break;
+                            case R.id.history:
+                                fragment = fragment2;
+                                break;
+                            case R.id.settings:
+                            default:
+                                fragment = fragment3;
+                                break;
+                        }
+                        fragmentManager.beginTransaction().replace(R.id.fragment_container,
+                                fragment).commit();
+                        return true;
+                    }
+                });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.homeFrag);
     }
 }
